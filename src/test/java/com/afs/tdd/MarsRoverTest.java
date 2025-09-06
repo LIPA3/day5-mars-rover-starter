309e144e0170945d9ba791b6cd2e02be99371564
Paige Li <LIPA3@oocl.com>
2025-09-06 12:26:31 +0800

    feat: add moveForward test

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -176,4 +176,64 @@ public class MarsRoverTest {
         assertEquals(1, marsRover.getY());
         assertEquals("N", marsRover.getDirection());
     }
+
+    @Test
+    public void should_return_x1_y0_N_when_x1_y1_command_B_direction_N(){
+        String direction = "N";
+        String command = "B";
+        int x = 1;
+        int y = 1;
+        MarsRover marsRover = new MarsRover(x, y, direction);
+
+        marsRover.executeCommand(command);
+
+        assertEquals(1, marsRover.getX());
+        assertEquals(0, marsRover.getY());
+        assertEquals("N", marsRover.getDirection());
+    }
+
+    @Test
+    public void should_return_x0_y0_S_when_x0_y1_command_B_direction_S(){
+        String direction = "S";
+        String command = "B";
+        int x = 0;
+        int y = 0;
+        MarsRover marsRover = new MarsRover(x, y, direction);
+
+        marsRover.executeCommand(command);
+
+        assertEquals(0, marsRover.getX());
+        assertEquals(1, marsRover.getY());
+        assertEquals("S", marsRover.getDirection());
+    }
+
+    @Test
+    public void should_return_x1_y0_W_when_x0_y0_command_B_direction_W(){
+        String direction = "W";
+        String command = "B";
+        int x = 0;
+        int y = 0;
+        MarsRover marsRover = new MarsRover(x, y, direction);
+
+        marsRover.executeCommand(command);
+
+        assertEquals(1, marsRover.getX());
+        assertEquals(0, marsRover.getY());
+        assertEquals("W", marsRover.getDirection());
+    }
+
+    @Test
+    public void should_return_x0_y0_E_when_x1_y0_command_B_direction_E(){
+        String direction = "E";
+        String command = "B";
+        int x = 1;
+        int y = 0;
+        MarsRover marsRover = new MarsRover(x, y, direction);
+
+        marsRover.executeCommand(command);
+
+        assertEquals(0, marsRover.getX());
+        assertEquals(0, marsRover.getY());
+        assertEquals("E", marsRover.getDirection());
+    }
 }
