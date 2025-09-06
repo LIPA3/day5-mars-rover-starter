f57e7d8eda8e8fd98854f1fabf97b9676b21c5ec
Paige Li <LIPA3@oocl.com>
2025-09-06 12:26:48 +0800

    feat: add moveForward method

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -5,9 +5,9 @@ import java.util.stream.IntStream;
 public class MarsRover {
     private int y;
     private int x;
-    private String command;
     public static final String LEFT_COMMAND = "L";
     public static final String RIGHT_COMMAND = "R";
+    public static final String BACKWARD_COMMAND = "B";
     public static final String NORTH = "N";
     public static final String WEST = "W";
     public static final String SOUTH = "S";
@@ -27,6 +27,9 @@ public class MarsRover {
                 break;
             case RIGHT_COMMAND:
                 turnRight();
+                break;
+            case BACKWARD_COMMAND:
+                moveBackward();
                 break;
             default:
                 moveForward();
@@ -53,6 +56,20 @@ public class MarsRover {
     private void moveForward() {
         int[] deltaX = {0, 1, 0, -1};
         int[] deltaY = {1, 0, -1, 0};
+        String[] directions = {NORTH, EAST, SOUTH, WEST};
+
+        IntStream.range(0, directions.length)
+                .filter(i -> directions[i].equals(direction))
+                .findFirst()
+                .ifPresent(i -> {
+                    x += deltaX[i];
+                    y += deltaY[i];
+                });
+    }
+
+    private void moveBackward() {
+        int[] deltaX = {0, -1, 0, 1};
+        int[] deltaY = {-1, 0, 1, 0};
         String[] directions = {NORTH, EAST, SOUTH, WEST};
 
         IntStream.range(0, directions.length)
